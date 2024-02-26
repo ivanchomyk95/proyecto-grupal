d6531f7c1560b70b53ab4925571e7d17da0a8cc0
EERomero <142105130+BulmaID@users.noreply.github.com>
2024-02-26 09:02:05 -0300

    prueba navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -17,7 +17,7 @@ function Navbar() {
         <ul className="flex space-x-4">
           <li className="relative">
             <button onClick={toggleDropdown} className="hover:text-gray-300 transition-colors duration-200">
-              Inicio
+              InicioPrueba
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 inline-block ml-1">
                 {dropdownVisible ? (
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
