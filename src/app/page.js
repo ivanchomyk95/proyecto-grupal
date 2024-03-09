459b481799ca44d5583b2d449c526df8e0d8e464
EERomero <142105130+BulmaID@users.noreply.github.com>
2024-03-09 11:54:25 -0300

    post-merge

--- src/app/page.js
+++ src/app/page.js
@@ -7,14 +7,6 @@
  *Definir nombre/logo
  *Definir idioma
  */
-"use client"
-
-import Navbar from "@/components/navbar/Navbar";
-import Card from "@/components/card/Card";
-
-import Beneficios from "@/components/beneficios/Beneficios";
-import Footer from "@/components/footer/footer";
-import Card from "@/components/card/Card";
 
 // import Navbar from "@/components/navbar/Navbar";
 // import Card from "@/components/card/Card";
@@ -91,83 +83,27 @@ import Card from "@/components/card/Card";
 /////////////////::::::::::::::::::::::::::::::::
 //npm install react-icons
 import Compromiso from "@/components/compromiso/Compromiso";
-import StyledButton from "@/components/button/Button";
 import Oferta from "@/components/oferta/Oferta";
 import ComicCon from "@/components/ComicCon/ComicCon";
 import Footer from "@/components/footer/footer";
+import Navbar from "@/components/navbar/Navbar";
+import Card from "@/components/card/Card";
+import Beneficios from "@/components/beneficios/Beneficios";
 
 //probando github
 
 export default function Home() {
   return (
     <div className="h-screen w-full">
-      <Beneficios />
-      <Card />
-      <Footer />
-    </div>
-    <>
       <Navbar />
-      <div className="bg-[#c5cacd57] mb-8 flex items-center">
-        <div className="flex flex-col justify-center flex-grow">
-          <h1 style={{
-      fontFamily: 'var(--font-family-title)',
-      fontSize: 'var(--font-size-title)',
-      fontWeight: 'var(--font-weight-title)',
-    }} className="text-center text-3xl font-semibold mb-4">
-            ¡Oferta Especial!
-          </h1>
-          <p className="text-sm text-center mb-4">
-            Aprovecha nuestras promociones exclusivas esta semana.
-          </p>
-          <div className="flex flex-col align-middle items-center pt-7">
-
-          <StyledButton texto="Promociones" />
-          </div>
-          
-        </div>
-        <div className="ml-auto">
-          <img src="/imgs/controladores.png" alt="Banner" />
-        </div>
-      </div>
-
-      <h2 style={{
-      fontFamily: 'var(--font-family-subtitle)',
-      fontSize: 'var(--font-size-subtitle)',
-      fontWeight: 'var(--font-weight-subtitle)',
-    }}className="text-center">Game Zone</h2>
-
-      <div className="flex justify-center my-16 mx-12 gap-35 mt-5 ">
-        <div className="flex justify-center space-x-14">
-          <Card
-            image="imgs/the-witcher.jpg"
-            title="The Witcher 3"
-            price="19.99"
-          />
-          <Card image="imgs/r-evil.jpg" title="R.E Village" price="24.99" />
-          <Card
-            image="imgs/serious-sam.jpg"
-            title="Serious Sam 4"
-            price="29.99"
-          />
-          <Card
-            image="imgs/batman-arkham.jpg"
-            title="Batman Arkham Knight"
-            price="14.99"
-          />
-        </div>
-      </div>
-
+      <Card />
+      <Beneficios />
       <div className="p-8">
         <Compromiso />
       </div>
-
-      <Beneficios />
-
-      <Oferta/>
-
-      <ComicCon/>
-    
-    </>
-
+      <Oferta />
+      <ComicCon />
+      <Footer />
+    </div>
   );
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,14 +4,14 @@ import StyledButton from "@/components/button/Button";
 
 const Card = () => {
   return (
-    <div className="overflow-hidden bg-f8f9fa rounded-md shadow-sm transition duration-300 transform hover:scale-105 mx-auto max-w-sm">
-      <img src={image} alt={title} className="w-full h-64 object-cover" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-700 text-base">${price}</p>
-        <div className="pt-4 pb-2">
-          <Boton texto="Comprar" />
-        </div>
+    // <div className="overflow-hidden bg-f8f9fa rounded-md shadow-sm transition duration-300 transform hover:scale-105 mx-auto max-w-sm">
+    //   <img src={image} alt={title} className="w-full h-64 object-cover" />
+    //   <div className="px-6 py-4"/>
+    //     <div className="font-bold text-xl mb-2">{title}</div>
+    //     <p className="text-gray-700 text-base">${price}</p>
+    //     <div className="pt-4 pb-2">
+    //       <Boton texto="Comprar" />
+    //     </div>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       <div className="bg-[#c5cacd57] max-w-sm rounded-3xl overflow-hidden shadow-xl mx-auto">
         <img src="imgs/the-witcher.jpg" alt="The Witcher" className="w-full h-auto" />
@@ -68,6 +68,7 @@ const Card = () => {
         </div>
       </div>
     </div>
+   
   );
 };
 
