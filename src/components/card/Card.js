50a4e47d05a2a005a91c7f489ac5a4367bb1f75a
ivan <ivanchomyk95@gmail.com>
2024-03-13 16:55:46 -0300

    mejora cards

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,7 +4,7 @@ import Boton from "../button/Button";
 const Card = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
-      <div className="bg-[#c5cacd57] max-w-sm rounded-3xl overflow-hidden shadow-xl mx-auto">
+      <div className="bg-[#c5cacd57] max-w-sm rounded-3xl overflow-hidden shadow-xl mx-auto transition-transform duration-300 ease-in-out hover:scale-105">
         <img src="imgs/the-witcher.jpg" alt="The Witcher" className="w-full h-auto" />
         <div className="px-6 py-4">
           <div className="mb-2 text-xl font-bold">The Witcher</div>
@@ -16,7 +16,7 @@ const Card = () => {
         </div>
       </div>
 
-      <div className="mx-auto max-w-sm overflow-hidden rounded-3xl bg-[#c5cacd57] shadow-xl">
+      <div className="mx-auto max-w-sm overflow-hidden rounded-3xl bg-[#c5cacd57] shadow-xl transition-transform duration-300 ease-in-out hover:scale-105" >
         <img
           src="imgs/r-evil.jpg"
           alt="Resident Evil"
@@ -32,7 +32,7 @@ const Card = () => {
         </div>
       </div>
 
-      <div className="mx-auto max-w-sm overflow-hidden rounded-3xl bg-[#c5cacd57] shadow-xl">
+      <div className="mx-auto max-w-sm overflow-hidden rounded-3xl bg-[#c5cacd57] shadow-xl transition-transform duration-300 ease-in-out hover:scale-105">
         <img
           src="imgs/serious-sam.jpg"
           alt="Serious Sam"
@@ -48,7 +48,7 @@ const Card = () => {
         </div>
       </div>
 
-      <div className="mx-auto max-w-sm overflow-hidden rounded-3xl bg-[#c5cacd57] shadow-xl">
+      <div className="mx-auto max-w-sm overflow-hidden rounded-3xl bg-[#c5cacd57] shadow-xl transition-transform duration-300 ease-in-out hover:scale-105">
         <img
           src="imgs/batman-arkham.jpg"
           alt="Batman Arkham"
