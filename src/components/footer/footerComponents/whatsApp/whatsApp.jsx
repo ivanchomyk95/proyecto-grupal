91a4b2fa763cbd975935209d5bdcd0708c90404a
Adrian <riviraadrian@gmail.com>
2024-03-13 16:00:57 -0300

    .

--- src/components/footer/footerComponents/whatsApp/whatsApp.jsx
+++ src/components/footer/footerComponents/whatsApp/whatsApp.jsx
@@ -6,11 +6,10 @@ export default function WhatsApp() {
   return (
     <div className={divContainer}>
       <a
-        className="text-3xl text-slate-100 lg:text-6xl lg:text-green-400 lg:hover:text-slate-100"
         href="https://wa.me/543704094513/?text=Hola%20estoy%20interesado%20en%20comprar%20juegos"
         target="_blank"
       >
-        <FaWhatsapp />
+        <FaWhatsapp className="text-3xl text-slate-100 lg:text-6xl lg:text-green-400 lg:hover:text-slate-100" />
       </a>
     </div>
   );
