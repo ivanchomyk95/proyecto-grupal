349609419510e7f8822aced2d02080d0d4a3b1f6
ivan <ivanchomyk95@gmail.com>
2024-03-19 16:13:09 -0300

    cards.01

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,8 +4,8 @@ import Boton from "../button/Button";
 const Card = () => {
   return (
    
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      <div className="bg-[#c5cacd57] max-w-sm rounded-3xl overflow-hidden shadow-xl mx-auto">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
+      <div className="bg-[#c5cacd57] max-w-sm rounded-3xl overflow-hidden shadow-xl mx-auto transition-transform duration-300 ease-in-out hover:scale-105">
         <img src="imgs/the-witcher.jpg" alt="The Witcher" className="w-full h-auto" />
         <div className="px-6 py-4">
           <div className="mb-2 text-xl font-bold">The Witcher</div>
